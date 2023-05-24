cf83da3e369bda7eb2e6824195e59e6a6aaed042
aziz-kacho <76108391+aziz-kacho@users.noreply.github.com>
2023-05-24 18:20:37 +0500

    clean code

--- app/src/main/java/com/example/filmscompose/feature_app/presentation/ui/components/LazyList.kt
+++ app/src/main/java/com/example/filmscompose/feature_app/presentation/ui/components/LazyList.kt
@@ -4,8 +4,6 @@ package com.example.filmscompose.feature_app.presentation.ui.components
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.lazy.LazyItemScope
 import androidx.compose.foundation.lazy.LazyListScope
-import androidx.compose.foundation.lazy.grid.LazyGridItemScope
-import androidx.compose.foundation.lazy.grid.LazyGridScope
 import androidx.compose.runtime.Composable
 import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
@@ -40,15 +38,5 @@ fun <T : Any> collectAsLazyPagingObserveItems(lazyItems: LazyPagingItems<T>): Re
 
 }
 
-@Composable
-fun <T : Any> collectAsLazyPagingObserve(lazyItems: LazyPagingItems<T>): LazyPagingItems<T>? {
-
-    return if (lazyItems.loadState.refresh is LoadState.NotLoading) {
-        lazyItems
-    } else {
-        null
-    }
-
-}
 
 
